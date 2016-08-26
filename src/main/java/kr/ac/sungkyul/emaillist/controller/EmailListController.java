e3ba59f51af64d38e60a56a7e8c8889ec9802308
S401-09 <S401-09@S401-09>
2016-08-26 15:53:16 +0900

    2016.08.26

---
+++ src/main/java/kr/ac/sungkyul/emaillist/controller/EmailListController.java
@@ -0,0 +1,51 @@
+package kr.ac.sungkyul.emaillist.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.ac.sungkyul.emaillist.dao.EmailListDao;
+import kr.ac.sungkyul.emaillist.vo.EmailListVo;
+
+@Controller
+public class EmailListController {
+
+	@Autowired
+	private EmailListDao dao;
+
+	// @RequestMapping("/list")
+	// public ModelAndView list() {
+	// List<EmailListVo> list = dao.getList();
+	//
+	// ModelAndView mav = new ModelAndView();
+	// mav.addObject("list", list);
+	// mav.setViewName("/WEB-INF/views/list.jsp");
+	//
+	// return mav;
+	// }
+
+	@RequestMapping("/list")
+	public String list(Model model) {
+		List<EmailListVo> list = dao.getList();
+
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/list.jsp";
+	}
+
+	@RequestMapping("/form")
+	public String form() {
+		return "/WEB-INF/views/form.jsp";
+	}
+
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public String insert(@ModelAttribute EmailListVo vo) {
+		dao.insert(vo);
+		return "redirect:/list";
+	}
+}
