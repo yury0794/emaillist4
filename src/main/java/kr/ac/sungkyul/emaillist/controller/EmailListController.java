e47bb1a7be8032c517fac998dd8e01537890ac9c
S401-09 <S401-09@S401-09>
2016-08-31 09:41:56 +0900

    2016.08.30

--- src/main/java/kr/ac/sungkyul/emaillist/controller/EmailListController.java
+++ src/main/java/kr/ac/sungkyul/emaillist/controller/EmailListController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.ac.sungkyul.emaillist.dao.EmailListDao;
 import kr.ac.sungkyul.emaillist.vo.EmailListVo;
